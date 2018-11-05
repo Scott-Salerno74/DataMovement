237fcde06f33bcbce191e8d71c9f473e748e1f84
sws001@lvc.edu <Xmas2002ss>
2018-11-05 11:58:27 -0500

    1

---
+++ src/InClassUnZip.java
@@ -0,0 +1,85 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class InClassUnZip {
+    static final int Threshold = 10;
+    static final int size = 10;
+    static double[] out1;
+    static double[] out2;
+    public static void parallelUnZip(double[]input){
+        int N = input.length;
+        if(N%2 ==0 ){
+            double[] out1 = new double[N/2];
+            double[] out2 = new double[N/2];
+        } else {
+            double[] out1 = new double[(N/2)+1];
+            double[] out2 = new double[N/2];
+        }
+        ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+        long start = System.nanoTime();
+        pool.invoke(new RecursiveUnZip(0,input.length,input,out1,out2));
+
+
+    }
+    private static boolean isUnZipCorrect(double[]input, double[] out1,double[] out2){
+        int n = 0;
+        int x;
+        for(int i =0; i <out1.length; i++){
+            if(out1[i] != input[i*2]){
+                return false;
+            }
+        }
+        for(int i=1; i <out2.length; i++){
+            if(out2[i] != input[(i*2)+1]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args){
+        double[] input = new double[size];
+        for(int i =0; i < input.length; i++){
+            input[i] = (Math.random()*10);
+        }
+        parallelUnZip(input);
+        System.out.println(isUnZipCorrect(input,out1,out2));
+
+    }
+    public static class RecursiveUnZip extends RecursiveAction {
+        int start;
+        int end;
+        double[] input;
+        double [] out1; // even outputs
+        double[] out2; // odd outputs
+        public RecursiveUnZip(int start, int end, double[] input , double [] out1, double[] out2){
+            this.start = start;
+            this.end = end;
+            this.input=input;
+            this.out1 = out1;
+            this.out2=out2;
+        }
+        @Override
+        protected void compute() {
+            if((end-start) < Threshold){
+                for( int i = start; i < end; i++){
+                    if(i%2 ==0){
+                        out1[i/2] = input[i];
+                    }
+                    else {
+                        out2[i/2] = input[i];
+                    }
+                }
+
+            }else{
+                int middle = (start-end)/2;
+                RecursiveUnZip unzipLeft = new RecursiveUnZip(start, middle, input, out1, out2);
+                unzipLeft.fork();
+                RecursiveUnZip unzipRight = new RecursiveUnZip(middle,end,input,out1,out2);
+                unzipRight.compute();
+                unzipLeft.join();
+
+            }
+        }
+    }
+}
---
+++ src/UnZIp.java
@@ -0,0 +1,107 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class UnZIp {
+    protected final static int size = 10;
+    protected final static int Threshold = 5;
+
+    /**
+     * Through splitting up the pool to run with
+     * each thread doing a separate portion I did notice some speed up
+     * From thread to thread.
+     * I have to find a better way to split up the data to notice even better Speed up between the Serial Version and the
+     * Parallel Version.
+     * I tested the code with arrays starting from size 10 to 1_000_000
+     * I can resubmit my code when and if I can find a better way to split up the work after I come and talk to you
+     */
+    protected static void poolUnZip(double[] input){
+        ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+        UnZip unZip = new UnZip(0,input);
+        pool.invoke(unZip);
+        UnZip unZip2 = new UnZip(1,input);
+        pool.invoke(unZip2);
+    }
+
+
+
+
+    protected static  void unZipGather( double[] input){
+
+        double[] output = new double[input.length];
+        double[] output2 = new double[input.length];
+        long start = System.nanoTime();
+        for(int i = 1; i < input.length; i=i+2){
+           //int y = index[i];
+            output[i] = input[i];
+
+        }
+        for(int i = 0; i < input.length; i=i+2){
+            output2[i] = input[i];
+        }
+       long end = System.nanoTime();
+        long total = (end-start)/1_000_000;
+        System.out.println("Serial Version Took:" +" "+total+" "+"seconds");
+        System.out.println("Thw Two unZipped Arrays are:");
+        System.out.println("First Array:");
+        for(double d: output){
+            System.out.println(d + ",");
+        }
+        System.out.println("Second Array:");
+        for(double x : output2){
+            System.out.println(x +",");
+        }
+    }
+
+    public static void main(String[] args){
+        double[] input = new double[size];
+        for(int i =0; i < input.length; i++){
+            input[i] = (Math.random()*10);
+        }
+
+         System.out.println("Original Array initialized");
+        unZipGather(input);
+//        for(double x : input){
+//            System.out.println(x+',');
+//        }
+//        System.out.println("Zipped pattern of the array:" +"\n");
+//        unZipGather(input);
+        poolUnZip(input);
+
+    }
+
+    protected static class UnZip extends RecursiveAction{
+         double[] input;
+         double[] output1;
+
+         int i;
+         int m;
+        public UnZip(int i, double[] input){
+            this.input = input;
+            output1 = new double[input.length];
+            this.i = i;
+            m = i+2;
+
+
+         }
+
+        @Override
+        protected void compute() {
+            if(input.length == 1){
+                output1[i] = input[i];
+            } else{
+                long start = System.nanoTime();
+                for(int i = this.i; i < input.length; i=i+2){
+                    output1[i] = input[i];
+                }
+                long end = System.nanoTime();
+                long total = (end-start)/100_000;
+                System.out.println("Took:" +" "+ total +" "+ "seconds");
+            }
+
+//          System.out.println("Unzipped Array:");
+//            for(double d : output1){
+//                System.out.println(d+',');
+//            }
+       }
+    }
+}
